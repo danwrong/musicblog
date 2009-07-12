4ca8d67a274ee52bf6d5f14b10dbf9a80d286110
Dan Webb <dan@danwebb.net>
2009-07-12 11:04:59 +0100

    added base methods to text converter

--- lib/text_converter.rb
+++ lib/text_converter.rb
@@ -1,5 +1,27 @@
+require 'hpricot'
+
 class TextConverter
   def convert(title, body)
+    title = convert_title(title)
+    body = convert_body(body)
+    
     return title, body
+  end
+  
+  protected
+  
+  def convert_title(title)
+    convert_snippet(title)
+  end
+  
+  def convert_body(body)
+    doc = Hpricot(body)
+    
+    doc.search('//text()').each do |snippet|
+      new_text = convert_snippet(snippet.inner_text)
+      snippet.swap(new_text)
+    end
+    
+    doc.to_html
   end
 end
